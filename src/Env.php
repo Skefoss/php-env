3ede353ed496293db3080af6b2a81768520d42a4
SIGUI Kessé Emmanuel <developer@sikessem.com>
2022-03-08 23:14:50 +0100

    :zap: Improve the Env class

--- src/Env.php
+++ src/Env.php
@@ -1,6 +1,6 @@
 <?php namespace Ske\Dot;
 
-class Env implements \ArrayAccess, \IteratorAggregate, \Countable {
+class Env implements \ArrayAccess, \IteratorAggregate, \Countable, \Stringable {
     public function __construct(array $vars = []) {
    		$this->setAll($vars);
     }
@@ -33,20 +33,6 @@ class Env implements \ArrayAccess, \IteratorAggregate, \Countable {
 		return $values;
 	}
 
-	public static function cast(array $data): self {
-		return new self($data);
-	}
-
-	public static function parse(mixed $value): mixed {
-		return match (true) {
-			in_array($value, ['null', 'Null', 'NULL', 'nil', 'Nil', 'NIL', 'none', 'NONE'], true) => null,
-			in_array($value, ['true', 'True', 'TRUE', 'on', 'On', 'ON', 'yes', 'Yes', 'YES', 't', 'T', 'y', 'Y', '1'], true) => true,
-			in_array($value, ['false', 'False', 'FALSE', 'off', 'Off', 'OFF', 'no', 'No', 'NO', 'f', 'F', 'n', 'N', '0'], true) => false,
-			is_numeric($value) => is_int(strpos($value, '.')) ? (float) $value : (int) $value,
-			default => $value,
-		};
-	}
-
     public function set(string $key, mixed $value): self {
         if (is_array($value)) {
 			foreach ($value as $k => $v) {
@@ -55,7 +41,7 @@ class Env implements \ArrayAccess, \IteratorAggregate, \Countable {
 		}
 		else {
 			$key = strtoupper($key);
-			$value = self::parse($value);
+			$value = self::convert($value);
 			putenv("$key=$value");
 			$this->vars[$key] = $_ENV[$key] = $_SERVER[$key] = $value;
 		}
@@ -77,6 +63,72 @@ class Env implements \ArrayAccess, \IteratorAggregate, \Countable {
     public function offsetExists(mixed $offset): bool {
         return $this->isset($offset);
     }
+
+	public static function parse(string|array $data): self {
+		if (is_string($data)) {
+			return is_file($data) ? self::parseFile($data) : self::parseData($data);
+		}
+		return new self($data);
+	}
+
+	public static function parseFile(string $file, bool $process_sections = false, int $flags = 0): self {
+		$data = \parse_ini_file($file, $process_sections, $flags);
+		if (false === $data) {
+			throw new \InvalidArgumentException("$file is not a valid INI file");
+		}
+		return self::parse($data);
+	}
+
+	public static function parseData(string $data, bool $process_sections = false, int $flags = 0): self {
+		$data = \parse_ini_string($data, $process_sections, $flags);
+		if (false === $data) {
+			throw new \InvalidArgumentException('Invalid INI data provided');
+		}
+		return self::parse($data);
+	}
+
+	public static function format(array $data, bool $process_sections = true): string {
+		$format = '';
+		foreach ($data as $key => $value) {
+			if (\is_array($value)) {
+				if (!$process_sections) {
+					throw new \InvalidArgumentException("Process sections is false, but $key is an array");
+				}
+				$format .= "[$key]" . PHP_EOL . self::format($value, false);
+			}
+			else {
+				$format .= $key . '=' . self::stringify($value, $process_sections) . PHP_EOL;
+			}
+		}
+		return $format;
+	}
+
+	public static function convert(mixed $value): mixed {
+		return match (true) {
+			in_array($value, ['null', 'Null', 'NULL', 'nil', 'Nil', 'NIL', 'none', 'NONE'], true) => null,
+			in_array($value, ['true', 'True', 'TRUE', 'on', 'On', 'ON', 'yes', 'Yes', 'YES', 't', 'T', 'y', 'Y', '1'], true) => true,
+			in_array($value, ['false', 'False', 'FALSE', 'off', 'Off', 'OFF', 'no', 'No', 'NO', 'f', 'F', 'n', 'N', '0'], true) => false,
+			is_numeric($value) => is_int(strpos($value, '.')) ? (float) $value : (int) $value,
+			default => $value,
+		};
+	}
+
+	public static function stringify(string|int|float|bool $value): string {
+		if (\is_bool($value)) {
+			$value = $value ? 'true' : 'false';
+		}
+		elseif (\is_string($value)) {
+			if (\preg_match('/^([\'"]).*\1$/', $value)) {
+				 return $value;
+			}
+			$value = false === \strpos($value, '\'') ? '\'' . $value . '\'' : '"' . $value . '"';
+			$value = \str_replace('\\', '\\\\', $value);
+		}
+		else {
+			$value = (string) $value;
+		}
+		return $value;
+	}
 
     public function offsetGet(mixed $offset): mixed {
         return $this->get($offset);
@@ -117,4 +169,12 @@ class Env implements \ArrayAccess, \IteratorAggregate, \Countable {
     public function __debugInfo(): array {
         return $this->vars;
     }
+
+    public function __toString(): string {
+		return self::format($this->vars);
+	}
+
+	public function __invoke(string $key, mixed $default = null): mixed {
+		return $this->get($key, $default);
+	}
 }
